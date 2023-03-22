bb07e4bae1bec5549e24d0a7f13c8986ee994719
Shubham Verma <shubhamv.sv@gmail.com>
2023-03-21 21:29:54 -0400

    Revert "enable xint always for jenkins compiling"
    This reverts commit 9c5c827a8a281c348a43ab043dc35a602af0a5f1.

--- runtime/vm/jvminit.c
+++ runtime/vm/jvminit.c
@@ -3192,8 +3192,7 @@ modifyDllLoadTable(J9JavaVM * vm, J9Pool* loadTable, J9VMInitArgs* j9vm_args)
 	 * -Xjit can co-exist with -Xnoaot and -Xaot, but overrides the others.
 	 * -Xint overrides everything.
 	 */
-	xint = TRUE;
-	xjit = xnojit = xnoaot = xaot = FALSE;
+	xint = xjit = xnojit = xnoaot = xaot = FALSE;
 	for (i=(vm_args->nOptions - 1); i>=0 ; i--) {
 		testString = getOptionString(j9vm_args, i);				/* may return mapped value */
 
