c793e9a7f698512463c2c6827dcc76b49b786678
Shubham Verma <shubhamv.sv@gmail.com>
2023-10-17 12:34:07 -0400

    Update getAddressOfElement to use dataAddr pointer
    Signed-off-by: Shubham Verma <shubhamv.sv@gmail.com>

--- runtime/compiler/env/J9ObjectModel.cpp
+++ runtime/compiler/env/J9ObjectModel.cpp
@@ -678,9 +678,21 @@ J9::ObjectModel::getAddressOfElement(TR::Compilation* comp, uintptr_t objectPoin
    TR_ASSERT(offset >= TR::Compiler->om.contiguousArrayHeaderSizeInBytes() &&
              offset < TR::Compiler->om.getArrayLengthInBytes(comp, objectPointer) + TR::Compiler->om.contiguousArrayHeaderSizeInBytes(), "Array is out of bound");
 
+   uintptr_t basePointer = objectPointer;
+   int64_t totalOffset = offset;
    // If the array is contiguous, return the addition of objectPointer and offset
+#if defined(J9VM_GC_ENABLE_SPARSE_HEAP_ALLOCATION)
+   if (TR::Compiler->om.isOffHeapAllocationEnabled())
+      {
+      basePointer = *(uintptr_t *)(objectPointer + TR::Compiler->om.offsetOfContiguousDataAddrField());
+      totalOffset = offset - static_cast<int32_t>(TR::Compiler->om.contiguousArrayHeaderSizeInBytes());
+      return basePointer + totalOffset;
+      }
+#endif /* J9VM_GC_ENABLE_SPARSE_HEAP_ALLOCATION */
    if (!TR::Compiler->om.isDiscontiguousArray(comp, objectPointer))
-      return objectPointer + offset;
+      {
+      return basePointer + totalOffset;
+      }
 
    // The following code handles discontiguous array
    //
